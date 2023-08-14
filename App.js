6b1def871af5b0c20ebadcbaf0ff4f1ebbb7f9cb
umangd98 <umangd98@gmail.com>
2023-08-14 13:30:46 -0500

    ep-1

---
+++ App.js
@@ -0,0 +1,24 @@
+
+const parent = React.createElement(
+    "div", {id:"parent"}, 
+    [React.createElement("div", 
+    {id:"child1"},
+     [React.createElement("h1", {}, "I'm an h1 tag"),
+     React.createElement("h2", {}, "I'm an h2 tag")  ]
+     ),
+     React.createElement("div", 
+     {id:"child2"},
+      [React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag")  ]
+      )]
+)
+
+// ReactElement(obj) => HTML(Browser Understandable)
+
+//created h1 tag using react 
+
+ console.log(parent) //return an object
+// createed root using reactdom - super parent
+const root = ReactDOM.createRoot(document.getElementById("root"))
+//take the react object adnd put it over the DOM. 
+root.render(parent)
