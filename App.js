8f17e318a10525f314317d2f67f20daf88f6879c
umangd98 <umangd98@gmail.com>
2023-08-25 20:21:55 -0500

    ep -2

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement(
     "div", {id:"parent"}, 
     [React.createElement("div", 
     {id:"child1"},
-     [React.createElement("h1", {}, "I'm an h1 tag"),
+     [React.createElement("h1", {}, "This is Namaste React "),
      React.createElement("h2", {}, "I'm an h2 tag")  ]
      ),
      React.createElement("div", 
